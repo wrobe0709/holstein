22373f4a3bd1fd5b696966f24fbdaeeb7fd06d5e
Will Robertson <wrobe0709@gmail.com>
2018-04-14 12:10:29 -0500

    initial commit

---
+++ app/pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Will Robertson'
+SITENAME = 'The Holstein House'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Full Stack Python', 'https://www.fullstackpython.com/'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+DISPLAY_PAGES_ON_MENU = True
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+PLUGINS = [
+    'pelican_fontawesome'
+]
