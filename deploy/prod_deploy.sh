1a6cb788d70faf2c4c91794721872a1b428b85e8
Will Robertson <wrobe0709@gmail.com>
2018-11-18 12:30:03 -0600

    update style and theme

--- deploy/prod_deploy.sh
+++ deploy/prod_deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -vvvv ./deploy.yml --private-key=/Users/Will/Desktop/Programming/holstein/keys/holstein_deploy -u deployer -i ./hosts
+ansible-playbook ./deploy.yml --private-key=/Users/Will/Desktop/Programming/holstein/keys/holstein_deploy -u deployer -i ./hosts
